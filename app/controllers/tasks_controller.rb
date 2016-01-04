cd60a5974070ebba87fd641d68b43f969217dbae
Andrew Fomera <andrew@zerlex.net>
2016-01-03 23:32:34 -0600

    Initial tasks model / implementation, just a basic task name. no complete action yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,50 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = current_user.tasks.order("updated_at ASC")
+  end
 
   def new
+    @task = current_user.tasks.build
   end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      redirect_to task_path(@task), notice: "Your task has been created!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task has been updated successfully!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task was removed successfully'
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 end
